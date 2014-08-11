4452fa5c79d4ced29907e9df5a65c591043f1ca8
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-08-11 18:30:39 +1200

    tests

--- verification/tests.py
+++ verification/tests.py
@@ -7,7 +7,6 @@ Each test is dict with
     "explanation" -- not necessary key, it's using for additional info in animation.
 """
 
-
 TESTS = {
     "Basics": [
         {
@@ -49,43 +48,36 @@ TESTS = {
         },
 
 
-
     ],
     "Extra": [
         {
-            "input": ["How are you doing?",
-                      "I'm fine. OK.",
-                      "Lorem Ipsum?",
-                      "Of course!!!",
-                      "1234567890",
-                      "0        0",
-                      "1234567890",
-                      "Fine! good buy!"],
-            "answer": 3,
-            "explanation": [[1, 9], [2, 5], [3, 2]],
+            "input": ['Lorem ipsum dolor', 'sit amet,', 'consectetuer', 'adipiscing elit.', 'Aenean commodo',
+                      'ligula eget dolor.', 'Aenean massa. Cum', 'sociis natoque', 'penatibus et magnis',
+                      'dis parturient', 'montes, nascetur', 'ridiculus mus. Donec', 'quam felis,', 'ultricies nec,',
+                      'pellentesque eu,', 'pretium quis, sem.', 'Nulla consequat', 'massa quis enim.',
+                      'Donec pede justo,', 'fringilla vel,'],
+            "answer": 11,
+            "explanation": [[1, 3], [3, 10], [5, 11], [8, 9], [8, 12], [9, 3], [10, 7], [11, 9], [12, 4], [13, 9],
+                            [15, 7]]
         },
         {
-            "input": ["WoW",
-                      "A A",
-                      "WoW"],
-            "answer": 1,
-            "explanation": [[1, 1]]
+            "input": ['qwerty asdfg ghjkl 123'],
+            "answer": 0,
+            "explanation": []
         },
         {
-            "input": ["HELLO",
-                      "O O O",
-                      "NIGHT",
-                      "O O O",
-                      "IDDQD"],
-            "answer": 4,
-            "explanation": [[1, 1], [1, 3], [3, 1], [3, 3]]
+            "input": ['asd', '0 1', 'asd', '0 1', 'asd', '0 1', 'asd', '0 1', 'asd', '0 1', 'asd', '0 1', 'asd', '0 1',
+                      'asd', '0 1', 'asd', '0 1', 'asd', '0 1'],
+            "answer": 9,
+            "explanation": [[1, 1], [3, 1], [5, 1], [7, 1], [9, 1], [11, 1], [13, 1], [15, 1], [17, 1]]
         },
         {
-            "input": ["HELLO",
-                      "O   O",
-                      "NIGHT",
-                      "O   O",
-                      "IDDQD"],
+            "input": ['hi hi', 'hello', 'hello', 'hello', 'hello', 'hello', 'by by'],
+            "answer": 0,
+            "explanation": []
+        },
+        {
+            "input": [' X ', 'XXX', ' X ', 'X X', ' X '],
             "answer": 0,
             "explanation": []
         },
